e81fbcc28fd3d45671ed505c494b8e9fa6b670dc
Ricardo <rmonroymtz1@gmail.com>
2021-07-13 15:18:57 -0500

    Added controllers to index song controller

--- src/controllers/song.controller/index.js
+++ src/controllers/song.controller/index.js
@@ -1,13 +1,19 @@
 "use strict";
 
+const deleteSong = require("./delete-song");
 const getSong = require("./get-song");
 const getSongs = require("./get-songs");
+const getSongFile = require("./get-song-file");
 const saveSong = require("./save-song");
 const updateSong = require("./update-song");
+const uploadSong = require("./upload-file-song");
 
 module.exports = {
+    deleteSong,
     getSong,
     getSongs,
+    getSongFile,
     saveSong,
     updateSong,
+    uploadSong,
 };
